3a9c6bd55e0982923f5c6896eb412af795ec35aa
Deependra Rajput <deependra.rajput@gmail.com>
2018-07-08 22:26:44 +1000

    Initial commit

---
+++ src/hackerrank/RestClient.java
@@ -0,0 +1,162 @@
+package hackerrank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.net.*;
+import com.google.gson.*;
+
+public class RestClient {
+	
+	   public static void main(String[] args) throws IOException{
+//	        Scanner in = new Scanner(System.in);
+//	        final String fileName = System.getenv("OUTPUT_PATH");
+//	        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+//	        String[] res;
+//	        String _substr;
+//	        try {
+//	            _substr = in.nextLine();
+//	        } catch (Exception e) {
+//	            _substr = null;
+//	        }
+		   
+		   String _substr = "spiderman";
+		   
+	        
+	        String[] res = getMovieTitles(_substr);
+	        for(int res_i=0; res_i < res.length; res_i++) {
+	            System.out.println(String.valueOf(res[res_i]));
+	        }
+	    }
+
+	/*
+	 * Complete the function below.
+	 */
+	
+	static String[] getMovieTitles(String substr) {
+		
+		List<String> allTitles = new ArrayList<String>();
+		
+		try {
+			MovieServiceReponse response = getMovieDetails(substr, null);
+			
+			response.getData().forEach(s->allTitles.add(s.getTitle()));
+			int pageToFetch = response.total_pages + 1;
+				while (pageToFetch --> 2) {
+					response = getMovieDetails(substr, pageToFetch);
+					response.getData().forEach(s->allTitles.add(s.getTitle()));
+				}
+		} catch (Exception e) {
+		}
+		
+		if(!allTitles.isEmpty()) Collections.sort(allTitles);
+	
+		String[] result = new String[allTitles.size()];
+		result = allTitles.toArray(result);
+		return result;
+	}
+	
+	private static MovieServiceReponse getMovieDetails(String title, Integer pageNumber) throws Exception {
+	
+		String url = "https://jsonmock.hackerrank.com/api/movies/search/?Title=" + title;
+		
+		if(pageNumber != null) {
+			url = url + "&page=" + pageNumber;
+		}
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+	
+		int responseCode = con.getResponseCode();
+		MovieServiceReponse response = null;
+		if(responseCode == 200) {
+			InputStreamReader in = new InputStreamReader(con.getInputStream());
+			response = new Gson().fromJson(in, MovieServiceReponse.class);
+			in.close();
+		}
+		return response;
+	}
+	
+	static class MovieServiceReponse {
+		private int page;
+		private int per_page;
+		private int total;
+		private int total_pages;
+		private List<MovieDetails> data;
+		public int getPage() {
+			return page;
+		}
+		public void setPage(int page) {
+			this.page = page;
+		}
+		public int getPer_page() {
+			return per_page;
+		}
+		public void setPer_page(int per_page) {
+			this.per_page = per_page;
+		}
+		public int getTotal() {
+			return total;
+		}
+		public void setTotal(int total) {
+			this.total = total;
+		}
+		public int getTotal_pages() {
+			return total_pages;
+		}
+		public void setTotal_pages(int total_pages) {
+			this.total_pages = total_pages;
+		}
+		public List<MovieDetails> getData() {
+			return data;
+		}
+		public void setData(List<MovieDetails> data) {
+			this.data = data;
+		}
+	}
+	
+	static class MovieDetails {
+	    private String Poster;
+	    private String Title;
+	    private String Type;
+	    private int Year;
+	    private String imdbID;
+		public String getPoster() {
+			return Poster;
+		}
+		public void setPoster(String poster) {
+			Poster = poster;
+		}
+		public String getTitle() {
+			return Title;
+		}
+		public void setTitle(String title) {
+			Title = title;
+		}
+		public String getType() {
+			return Type;
+		}
+		public void setType(String type) {
+			Type = type;
+		}
+		public int getYear() {
+			return Year;
+		}
+		public void setYear(int year) {
+			Year = year;
+		}
+		public String getImdbID() {
+			return imdbID;
+		}
+		public void setImdbID(String imdbID) {
+			this.imdbID = imdbID;
+		}
+	}
+
+
+
+}
---
+++ src/hackerrank/SortAsBinary.java
@@ -0,0 +1,57 @@
+package hackerrank;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SortAsBinary {
+	
+	public static void main(String[] args) {
+		List<Integer> input = Arrays.asList(1, 5, 76, 3, 2, 4, 8, 15, 31, 63, 3 , 7, 9);
+		
+		System.out.println(rearrange(input));
+	}
+	
+	private static List<Integer> rearrange(List<Integer> elements) {
+
+		elements.sort((i1, i2) -> {
+			int one1 = countMethodOne(i1);	//Alternatively using first method
+			int one2 = countMethodTwo(i2);	//Alternatively using second method
+			
+			return (one1 == one2)? i1-i2: one1-one2;
+		});
+
+		return elements;
+		
+    }
+	
+	/**
+	 * First method. Takes ~20000 ms on Long.MAX_VALUE for 100_000_000 times
+	 * @param s
+	 * @return
+	 */
+	private static int countMethodOne(long n) {
+		String s = Long.toBinaryString(n);
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+		    if (s.charAt(i) == '1') {
+		        count++;
+		    }
+		}
+		return count;
+	}
+	
+	/**
+	 * Second method. Takes ~28000 ms on Long.MAX_VALUE for 100_000_000 times
+	 * @param s
+	 * @return
+	 */
+	private static int countMethodTwo(long n) {
+		int count = 0;
+		while(n > 0) {
+			count+=n%2;
+			n/=2;
+		}
+		return count;
+	}
+
+}
