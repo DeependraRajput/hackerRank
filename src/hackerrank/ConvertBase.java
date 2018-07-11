45169d02e6b05fbfe742446f79bd2fd7ac2fe7e3
Deependra Rajput <deependra.rajput@gmail.com>
2018-07-11 20:55:51 +1000

    Conversion to and fro

--- src/hackerrank/ConvertBase.java
+++ src/hackerrank/ConvertBase.java
@@ -2,40 +2,76 @@ package hackerrank;
 
 public class ConvertBase {
 	
+	private static final String CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
 	public static void main(String[] args) {
-		System.out.println(convert(11, 7));
-		System.out.println(convertToBaseString(25, 14));
+		long origNum = 124;
+		int base = 2;
+		String otherBase = convertToBaseString(origNum, base);
+		System.out.println("Number in base [" + base + "] is [" + otherBase + "]");
+		long backNum = convertFromBaseString(otherBase, base);
+		System.out.println("Original number in decimal [" + origNum + "] Converted number in decimal [" + backNum + "]");
+		
 	}
 	
 	/**
-	 * Approach one to keep result in numeric. Caveat is result should not be beyond 2147483647 (Long.MAX_VALUE). and base must be less than 10
-	 * Lower the base, the bigger the result.
+	 * Convert the long number in decimal to the given base in String type.
+	 * @param num
+	 * @param base
+	 * @return
 	 */
-	private static long convert(int num, int base) {
-		long result = 0;
-		
-		int p = 0;
+	private static String convertToBaseString(long num, int base) {
+		checkBase(base);
+		StringBuilder sb = new StringBuilder();
 		while(num > 0) {
-			result = (long) ((num % base) * java.lang.Math.pow(10, p++) + result);
+			sb.append(CHARSET.charAt((int)num % base));
 			num /= base;
+		}
+		
+		return sb.reverse().toString();
+	}
+	
+	/**
+	 * Convert the String number in the given base to decimal in long type.
+	 * @param numStr
+	 * @param base
+	 * @return
+	 */
+	private static long convertFromBaseString(String numStr, int base) {
+		checkBase(base);
+		
+		long result = 0;
+		for(char c: numStr.toCharArray()) {
+			result = result*base + CHARSET.indexOf(c);
 		}
 		
 		return result;
 	}
 	
 	/**
-	 * Approach two to keep result in string to accommodate any string.
-	 * Caveat, the base must be less than the charset defined. Or extend the charset.
+	 * Check if the base is supported by the given character set.
+	 * @param base
 	 */
-	private static String convertToBaseString(int num, int base) {
-		final char[] charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-		StringBuilder sb = new StringBuilder();
-		while(num > 0) {
-			sb.append(charset[(int)num % base]);
-			num /= base;
-		}
-		
-		return sb.reverse().toString();
+	private static void checkBase(int base) {
+		if(base > CHARSET.length()) throw new IllegalArgumentException("Base too large than support");
 	}
+	
+	
+	/**
+	 * JUST SAMPLE: Approach one to keep result in numeric. 
+	 * Caveat is result should not be beyond 2147483647 (Long.MAX_VALUE) and base must be less than 10.
+	 * Lower the base, the bigger the result.
+	 */
+//	private static long convertDecimalToOtherBase(int num, int base) {
+//		long result = 0;
+//		
+//		int p = 0;
+//		while(num > 0) {
+//			result = (long) ((num % base) * java.lang.Math.pow(10, p++) + result);
+//			num /= base;
+//		}
+//		
+//		return result;
+//	}
 
 }
